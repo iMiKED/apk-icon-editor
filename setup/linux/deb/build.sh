1aad7096d339b873903ceeb6ceae04d0cf524d0d
iMiKE <M-I-B@yandex.ru>
2026-04-27 03:35:33 +0700

    Add GitHub Actions release builds

--- setup/linux/deb/build.sh
+++ setup/linux/deb/build.sh
@@ -3,17 +3,20 @@
 export DEBEMAIL="imiked@users.noreply.github.com"
 export DEBFULLNAME="iMiKED"
 
+PACKAGE="apk-icon-editor"
+VERSION="3.0.0~beta1"
+
 clean() {
     rm ../apk-icon-editor_*.tar.xz ../apk-icon-editor_*.tar.gz ../apk-icon-editor_*.dsc ../apk-icon-editor_*.changes ../apk-icon-editor_*.build 2> /dev/null
     rm -rf ./debian 2> /dev/null
 }
 
-cd ../../..
+cd "$(dirname "$0")/../../.."
 
 clean
 rm ../apk-icon-editor_*.deb 2> /dev/null
 
-if dh_make -y -s -c gpl3 --createorig; then
+if dh_make -y -s -c gpl3 --createorig --packagename "$PACKAGE"_"$VERSION"; then
     rm -rf ./debian 2> /dev/null
     cp -R ./setup/linux/deb/debian ./debian
     debuild -uc -us
